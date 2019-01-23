f88d90b80c85e527f9287ce6241872269adf02ea
michael <EX-XIAOZHIFEN001>
2019-01-23 16:35:44 +0800

    no message

--- daydayFresh/test.py
+++ daydayFresh/test.py
@@ -1,7 +1,11 @@
 
 class test():
     def __init__(self):
+<<<<<<< HEAD
         self.name = 'xiao'
+=======
+        self.name = "xiao"
+>>>>>>> parent of 8812fd2... 2
         self.age = 22
 
 a = test()
